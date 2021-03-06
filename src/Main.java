2c18436a4f3e23693af25e91a4091b7d1628bfec
Oleg Dasheev <odasheyev@mail.ru>
2021-03-06 14:56:32 +0800

    Added src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int transfer = 500_000_000;
+        int total = transfer / balance;
+        System.out.println("total: " + total);
+    }
 }
